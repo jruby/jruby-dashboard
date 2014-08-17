f1427ade3c1bf5473c449cd3edc6b9fc96064566
Charles Oliver Nutter <headius@headius.com>
2014-08-17 12:22:15 -0400

    Move jmx_prep into config.ru.

--- lib/jmx_prep.rb
+++
@@ -1,8 +0,0 @@
-require 'jmx'
-
-server = JMX.simple_server
-client = JMX.connect
-$memory = client["java.lang:type=Memory"]
-$jruby_parser = client["org.jruby:type=Runtime,name=0,service=ParserStats"]
-$jruby_runtime = client["org.jruby:type=Runtime,name=0,service=Runtime"]
-
