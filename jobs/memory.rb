a40b30f741ed09d939edae23e43da7adb08f2341
Charles Oliver Nutter <headius@headius.com>
2014-08-17 12:57:20 -0400

    Only show last ten minutes of heap sizes.

--- jobs/memory.rb
+++ jobs/memory.rb
@@ -5,7 +5,9 @@ SCHEDULER.every '1s' do
   heap_bean = $memory.heap_memory_usage
   time = Time.now.to_i
   heap_size << {"x" => time, "y" => (heap_bean.committed / 1024 / 1024)}
+  heap_size.shift until heap_size.size <= 600
   heap_usage << {"x" => time, "y" => (heap_bean.used / 1024 / 1024)}
+  heap_usage.shift until heap_usage.size <= 600
   send_event('heap_size', points: heap_size)
   send_event('heap_usage', points: heap_usage)
 end
